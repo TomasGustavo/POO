daa127e0e0168956d8ba14f3abac4ed60401ed73
TomasGustavo <101200943+ImKyrio@users.noreply.github.com>
2023-08-18 00:41:57 -0300

    -new functions eliminar\recuperar\insertar\longitud\lista vacia

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -5,8 +5,32 @@ public class Main {
 
         ListaEnlazada lista = new ListaEnlazada();
 
-        lista.agregar("Hola ");
-        lista.agregar("mundo!");
-        System.out.println(lista);
+        // metodo de agregar
+        System.out.println(lista.vacia());
+        System.out.println(lista.longitud());
+        for(int i = 0; i<8;i++){
+            lista.agregar(i);
+        }
+        System.out.print(lista);
+        System.out.println("------------------");
+
+        // metodo eliminar (por dato)
+        lista.eliminar(4);
+        lista.eliminar(6);
+        System.out.println(lista.longitud());
+        System.out.println("------------------");
+
+        // metodo recuperar (por por posicion logica)
+        System.out.print("Dato recuperado: - ");
+        System.out.println(lista.recuperar(0));
+        System.out.println("------------------");
+
+        // metodo insertar por posicion (logica)
+        boolean rta = lista.insertarPosicion(5,10);
+        System.out.println(rta);
+        System.out.print(lista);
+        System.out.println(lista.longitud());
+        System.out.println("------------------");
+
     }
 }
--- TP1/src/ar/edu/unlu/poo/lista/ListaEnlazada.java
+++ TP1/src/ar/edu/unlu/poo/lista/ListaEnlazada.java
@@ -1,10 +1,10 @@
 package ar.edu.unlu.poo.lista;
 
-import javax.print.attribute.standard.JobKOctets;
 
 public class ListaEnlazada {
     // Atributos
     private Nodo inicio = null;
+    private int cantidad = 0;
 
     // Metodos
     public void agregar(Object dato){
@@ -12,13 +12,81 @@ public class ListaEnlazada {
         nuevoNodo.setDato(dato);
         if(inicio == null){
             inicio = nuevoNodo;
+            cantidad++;
         } else{
             Nodo nodoAux = inicio;
             while(nodoAux.getProximo() != null){
                 nodoAux = nodoAux.getProximo();
             }
             nodoAux.setProximo(nuevoNodo);
+            cantidad++;
         }
+    }
+
+    public void eliminar(Object dato){
+        Nodo nodoAux = inicio;
+        if(nodoAux.getDato() == dato) {
+            inicio = nodoAux.getProximo();
+        } else {
+            while (nodoAux.getProximo().getDato() != dato){
+                nodoAux = nodoAux.getProximo();
+            }
+            nodoAux.setProximo(nodoAux.getProximo().getProximo());
+        }
+        cantidad--;
+    }
+
+    public Object recuperar(int posicion){
+        Object rta;
+        int posiAux = 0;
+        if(vacia()){
+            rta = "La lista esta vacia";
+            return rta;
+        } else if((posicion < 0) || posicion >= cantidad){
+            rta = "posicion fuera de rango!!!!!";
+            return rta;
+        }
+        Nodo actual = inicio;
+        while(posiAux != posicion){
+            actual = actual.getProximo();
+            posiAux++;
+        }
+        rta = actual.getDato();
+        return rta;
+    }
+
+    public boolean insertarPosicion(int posicion, Object dato){
+        boolean rta;
+        if ((posicion > cantidad) || (posicion < 0) || (vacia())){
+            rta = false;
+        } else {
+            Nodo nuevoNodo = new Nodo();
+            nuevoNodo.setDato(dato);
+            Nodo nodoAux = inicio;
+            if(posicion == 0){
+                inicio = nuevoNodo;
+                nuevoNodo.setProximo(nodoAux);
+                rta = true;
+            } else{
+                int i = 0;
+                while ((i+1) != posicion){
+                    nodoAux = nodoAux.getProximo();
+                    i++;
+                }
+                nuevoNodo.setProximo(nodoAux.getProximo());
+                nodoAux.setProximo(nuevoNodo);
+                rta = true;
+            }
+        }
+        cantidad++;
+        return rta;
+    }
+
+    public boolean vacia(){
+        return (cantidad == 0);
+    }
+    public int longitud(){
+        return cantidad;
     }
 
     public String toString(){
